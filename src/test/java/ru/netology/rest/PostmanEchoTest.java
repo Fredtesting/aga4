433f8e64d514ca6cbb66d1d3aac3712df080eed8
Voronchikhin Fedor <fredsergeevich@outlook.com>
2022-12-17 22:51:43 +0300

    Postman Test

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,37 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldNotReturnData() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Data")
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo ("Nata"));
+    }
+    @Test
+    void shouldReturnData() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Data")
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo ("Data"));
+    }
+}
